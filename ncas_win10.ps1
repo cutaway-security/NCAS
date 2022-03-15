c2ea62264e617c9956bc31cfa29bb1b24cafdb45
cutaway <cutawaysecurity@gmail.com>
2022-03-15 08:18:27 -0500

    Update ncas_win10.ps1

--- ncas_win10.ps1
+++ ncas_win10.ps1
@@ -1,58 +1,39 @@
-<#
-	ncas_win10.ps1 - NERC CIP Audit Script for Windows 10. This script 
-                     will collect data from the system and generate a
-                     text and HTML report file, and provide individual
-                     output files.
-    Author: Don C. Weber (@cutaway)
-    Date:   March 15, 2022
-#>
 
-<#
-	License: 
-	Copyright (c) 2022, Cutaway Security, Inc. <dev [@] cutawaysecurity.com>
-	
-	sawh.ps1 is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	sawh.ps1 is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	Point Of Contact:    Don C. Weber <dev [@] cutawaysecurity.com>
-#>
-
-#############################
-# Set up document header information
-#############################
-$script_name         = 'ncas_win10'
-$script_version      = '1.0'
-$consultant_company  = 'Cutaway Security, LLC'
-$company_lg          = 'ACME, Inc.'
-$company_sh          = 'ACME'
-$sitename            = 'plant1'
-$start_time          = Get-Date -format yyyyMMddHHmmssff
-$start_time_readable = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
-$computername        = $env:ComputerName
-$runname             = $company_sh + '_' + $start_time
-#$outdir              = "$env:APPDATA\$runname"
-$outdir              = "C:\Users\student\Desktop\test\$runname"
-$outfile             = $computername + '_NCAS_report_' + $start_time + '.txt'
-$outfile_stub        = $computername + '_' + $start_time
-$outpath             = "$outdir\$outfile"
-$outpath_stub        = "$outdir\$outfile_stub"
 
 #############################
 # Script behavior parameters
 #############################
 $runstat             = $true # change to disable run status output
 $cutsec_footer       = $true # change to false to disable CutSec footer
+$consultant_company  = 'Cutaway Security, LLC'
+$company_lg          = 'ACME, Inc.'
+$company_sh          = 'ACME'
+$sitename            = 'plant1'
 
+#############################
+# Set up document header information
+#############################
+$script_name         = 'ncas_win10'
+$script_version      = '1.0'
+$start_time          = Get-Date -format yyyyMMddHHmmssff
+$start_time_readable = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
+$computername        = $env:ComputerName
+$runname             = $company_sh + '_' + $start_time
+$outdir              = "$env:APPDATA\$runname"
+$outfile             = $computername + '_NCAS_report_' + $start_time + '.txt'
+$outhtml             = $computername + '_NCAS_report_' + $start_time + '.html'
+$outfile_stub        = $computername + '_' + $start_time
+$outpath             = "$outdir\$outfile"
+$outpath_stub        = "$outdir\$outfile_stub"
+$outpath_html        = "$outdir\$outhtml"
 
-
+$Header = @"
+<style>
+TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;}
+TH {border-width: 1px; padding: 3px; border-style: solid; border-color: black; background-color: #6495ED;}
+TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+</style>
+"@
 
 #############################
 # Start script
@@ -72,14 +53,20 @@ else
 #############################
 # Document Header
 #############################
-Write-Output "# $company_sh NCAS Data: $computername" | Out-File -FilePath $outpath -Append
+$report_name = "$company_sh NCAS Data: $computername`n"
 
-Write-Output "`n## Computer Information`n" | Out-File -FilePath $outpath -Append
-Write-Output "System Name: $computername" | Out-File -FilePath $outpath -Append
-Write-Output "Company: $company_lg" | Out-File -FilePath $outpath -Append
-Write-Output "Assessment Team: $consultant_company" | Out-File -FilePath $outpath -Append # comment this out or rename to team name
-Write-Output "Script Version: $script_name $script_version" | Out-File -FilePath $outpath -Append
-Write-Output "Start Time: $start_time_readable" | Out-File -FilePath $outpath -Append
+$header_name   = "`nComputer Information`n"
+$report_header = "System Name: $computername`n"
+$report_header += "Company: $company_lg`n"
+$report_header += "Assessment Team: $consultant_company`n" # comment this out or rename to team name
+$report_header += "Script Version: $script_name $script_version`n"
+$report_header += "Start Time: $start_time_readable`n"
+
+$report_name | Out-File -FilePath $outpath -Append
+$report_name_HTML = "<h1>$report_name</h1>"
+$header_name+$report_header | Out-File -FilePath $outpath -Append
+#$report_header_HTML = $report_header | ConvertTo-Html -Fragment -PreContent "<h2>Computer Information</h2>"
+$report_header_HTML = "<h2>$header_name</h2><p>" + $report_header.Replace("`n","<br>") + "</p>"
 
 #############################
 # Gather inforamtion about computer version
@@ -90,7 +77,9 @@ if ($runstat) {Write-Host "Gathering computer version information"}
 $sysinfo = Get-ComputerInfo -Property WindowsProductName,OsVersion,WindowsCurrentVersion,WindowsVersion
 $sysinfo | Format-Table -AutoSize | Out-File -FilePath $outpath -Append
 $sysinfo | Format-Table -AutoSize | Out-File -FilePath $outpath_stub"_sysinfo.txt" -Append
+$sysinfo_HTML = $sysinfo | ConvertTo-Html -Fragment -PreContent "<h2>Computer Version Information</h2>"
 
+# Output the old systeminfo file which can be feed into WES-NG for patch vulnerability information
 $sysinfo_orig = systeminfo
 $sysinfo_orig | Format-Table -AutoSize | Out-File -FilePath $outpath_stub"_orig_sysinfo.txt" -Append
 
@@ -100,9 +89,10 @@ $sysinfo_orig | Format-Table -AutoSize | Out-File -FilePath $outpath_stub"_orig_
 Write-Output "`n## Security Patch Information`n" | Out-File -FilePath $outpath -Append
 if ($runstat) {Write-Host "Gathering security patch information"}
 
-$hotfixes = Get-Hotfix | Format-Table PSComputerName,Description,HotFixID,InstalledOn -AutoSize 
-$hotfixes | Out-File -FilePath $outpath -Append
-$hotfixes | Format-Table -AutoSize | Out-File -FilePath $outpath_stub"_hotfixes.txt" -Append
+$hotfixes = Get-Hotfix 
+$hotfixes | Format-Table PSComputerName,Description,HotFixID,InstalledOn -AutoSize | Out-File -FilePath $outpath -Append
+$hotfixes | Format-Table PSComputerName,Description,HotFixID,InstalledOn -AutoSize | Out-File -FilePath $outpath_stub"_hotfixes.txt" -Append
+$hotfixes_HTML = $hotfixes | ConvertTo-Html -Property PSComputerName,Description,HotFixID,InstalledOn -Fragment -PreContent "<h2>Security Patch Information</h2>"
 
 #############################
 # Gather information about installed software
@@ -138,8 +128,13 @@ foreach($key in $subkeys){
     $array += $obj
 } 
 
-$array | Where-Object { $_.DisplayName } | select ComputerName, DisplayName, DisplayVersion, Publisher | Format-Table -AutoSize | Out-File -FilePath $outpath -Append
-$array | Where-Object { $_.DisplayName } | select ComputerName, DisplayName, DisplayVersion, Publisher | Format-Table -AutoSize | Out-File -FilePath $outpath_stub"_software.txt" -Append
+$software_versions = $array | Where-Object { $_.DisplayName } `
+ | select ComputerName, DisplayName, DisplayVersion, Publisher `
+ | Format-Table -AutoSize | Out-String -Width 4096
+
+$software_versions | Out-File -FilePath $outpath -Append
+$software_versions | Out-File -FilePath $outpath_stub"_software.txt" -Append
+$software_HTML = $array | Where-Object { $_.DisplayName } | ConvertTo-Html -Fragment -PreContent "<h2>Installed Software Information</h2>"
 
 #############################
 # Gather inforamtion about TCP and UDP Listening Services
@@ -147,6 +142,7 @@ $array | Where-Object { $_.DisplayName } | select ComputerName, DisplayName, Dis
 #############################
 Write-Output "`n## Network Connection Information`n" | Out-File -FilePath $outpath -Append
 if ($runstat) {Write-Host "Gathering TCP and UDP Listening Services"}
+$net_servers_HTML = "<h2>Network Connection Information</h2>"
 
 # Make a lookup table by process ID
 $Processes = @{}
@@ -155,8 +151,8 @@ Get-Process | ForEach-Object {
 }
 
 # Query Listening TCP Daemons
-Write-Output "### TCP Daemons" | Out-File -FilePath $outpath -Append
-Write-Output "# TCP Daemons" | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
+Write-Output "### TCP Network Servers" | Out-File -FilePath $outpath -Append
+Write-Output "# TCP Network Servers" | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
 $tcpservers = Get-NetTCPConnection | 
     Where-Object { $_.State -eq "Listen" -and $_.LocalAddress -ne "127.0.0.1" } |
     Select-Object LocalAddress,
@@ -164,10 +160,10 @@ $tcpservers = Get-NetTCPConnection |
         @{Name="PID";         Expression={ $_.OwningProcess }},
         @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }}, 
         @{Name="Path"; Expression={ $Processes[[int]$_.OwningProcess].Path }} |
-    Sort-Object -Property LocalPort, LocalAddress |
-    Format-Table -AutoSize 
-$tcpservers | Out-File -FilePath $outpath -Append
-$tcpservers | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
+    Sort-Object -Property LocalPort, LocalAddress 
+$tcpservers | Format-Table -AutoSize | Out-String -Width 4096 | Out-File -FilePath $outpath -Append
+$tcpservers | Format-Table -AutoSize | Out-String -Width 4096 | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
+$tcp_servers_HTML = $tcpservers | ConvertTo-Html -Fragment -PreContent "<h3>TCP Network Servers</h3>"
 
 # Query Listening UDP Daemons
 Write-Output "### UDP Daemons" | Out-File -FilePath $outpath -Append
@@ -179,28 +175,40 @@ $udpservers = Get-NetUDPEndpoint |
         @{Name="PID";         Expression={ $_.OwningProcess }},
         @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }}, 
         @{Name="Path"; Expression={ $Processes[[int]$_.OwningProcess].Path }} |
-    Sort-Object -Property LocalPort, LocalAddress |
-    Format-Table -AutoSize | Out-File -FilePath $outpath -Append
-$udpservers | Out-File -FilePath $outpath -Append
-$udpservers | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
+    Sort-Object -Property LocalPort, LocalAddress
+$udpservers | Format-Table -AutoSize | Out-String -Width 4096 | Out-File -FilePath $outpath -Append
+$udpservers | Format-Table -AutoSize | Out-String -Width 4096 | Out-File -FilePath $outpath_stub"_network_services.txt" -Append
+$udp_servers_HTML = $udpservers | ConvertTo-Html -Fragment -PreContent "<h3>UDP Network Servers</h3>"
 
 # Footer
 Write-Output "## Script Completed`n" | Out-File -FilePath $outpath -Append
 $stop_time_readable = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
-Write-Output "NCAS run completed at $stop_time_readable" | Out-File -FilePath $outpath -Append
+$completed = "NCAS run completed at $stop_time_readable"
+Write-Output $completed | Out-File -FilePath $outpath -Append
+
+$completed_HTML = "<h2>Script Completed</h2><p>" + $completed + "</p>" 
+
+#############################
+# Cutaway Security Footer
+#############################
+
+$cutsec_footer =  "****************************************************`n"
+$cutsec_footer += "NCAS is brought to you by Cutaway Security, LLC`n"
+$cutsec_footer += "For assistance with your assessments, please contact info [@] cutawaysecurity.com`n"
+$cutsec_footer += "For recommendations or issues, please add an issues or create a pull request on GitHub, or contact dev [@] cutawaysecurity.com`n"
+$cutsec_footer += "****************************************************`n"
+
+$cutsec_footer_HTML = '<p></p>'
+if ($cutsec_footer){
+    $cutsec_footer | Out-File -FilePath $outpath -Append
+    $cutsec_footer_HTML = "<p>" + $cutsec_footer.Replace("`n","</br>") + "</p>"
+}
 
 #############################
 # Stop script
 #############################
 if ($runstat) {Write-Host "NCAS run finished at $stop_time_readable on $computername"}
-
-#############################
-# Cutaway Security Footer
-#############################
-if ($cutsec_footer -eq $true){
-    Write-Output "`n>              *************                     " | Out-File -FilePath $outpath -Append
-    Write-Output "> NCAS is brought to you by Cutaway Security, LLC" | Out-File -FilePath $outpath -Append
-    Write-Output "> For assistance with your assessments, please contact info [@] cutawaysecurity.com" | Out-File -FilePath $outpath -Append
-    Write-Output "> For recommendations or issues, please add an issues or create a pull request on GitHub, or contact dev [@] cutawaysecurity.com" | Out-File -FilePath $outpath -Append
-    Write-Output ">              *************                     " | Out-File -FilePath $outpath -Append
-}
+$report_HTML = ConvertTo-HTML -Head $Header -Body `
+ "$report_name_HTML $report_header_HTML $sysinfo_HTML $hotfixes_HTML $software_HTML $net_servers_HTML $tcp_servers_HTML $udp_servers_HTML $completed_HTML $cutsec_footer_HTML" `
+ -Title "$report_name" -PostContent "<p>Creation Date: $stop_time_readable<p>"
+$report_HTML | Out-File -FilePath $outpath_html 
